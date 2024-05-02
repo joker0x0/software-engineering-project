98c118038867f1084b4e8738e84b934bbec66125
joker0x0 <pro.ha.cker909009@gmail.com>
2024-05-02 16:06:38 -0700

    this is the setup part

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const app = express()
+const mongoose = require('mongoose');
+
+require('dotenv').config()
+
+app.use(express.static('public'));
+app.use(express.json());
+
+
+mongoose.connect(process.env.dbURI, { useNewUrlParser: true, useUnifiedTopology: true})
+  .then((result) => {
+    console.log('DB Connected')
+    app.listen(process.env.PORT, () => {
+        console.log(`Server is running on port: ${process.env.PORT}`);
+    })
+  })
+  .catch((err) => console.log(err));
+
