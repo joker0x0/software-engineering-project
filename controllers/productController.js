2157ee63a41e2b7ce2a5e4ae8a96fa178fc50a0f
abdelrhman123h <Bedot9467@gmail.com>
2024-05-02 22:57:54 +0300

    Admin Delete product

--- controllers/productController.js
+++ controllers/productController.js
@@ -8,6 +8,21 @@ const addProduct = async (req,res)=>{
     
 }
 const deleteProduct = async(req,res)=>{
+        const id= req.body.id;
+    //find Index to check if it is in the database or not
+    const Index=await productModel.findById(id);
+
+    if(Index){
+        //remove product from the array
+        await productModel.findByIdAndDelete(id);
+        console.log("item with ID ${prproductModelID} is deleted succesfully");
+        return res.status(200).json("item with ID ${prproductModelID} is deleted succesfully");
+    }
+    else {
+        console.log("Item with ID ${productModelID} not found");
+        return res.status(400).jason("Item with ID ${productModelID} not found");
+    }
+
 
 }
 const updateProduct = async(req,res)=>{
@@ -15,5 +30,7 @@ const updateProduct = async(req,res)=>{
 }
 
 module.exports={
-    addProduct
+    addProduct,
+    deleteProduct,
+    updateProduct  
 }
--- routes/productRouter.js
+++ routes/productRouter.js
@@ -10,8 +10,7 @@ const { auth ,Roles } = require("../middlewares/authMiddleware");
 
 router.post("/addProduct",auth([Roles.Admin]),addProduct)
 
-
-//router.post('/removeProduct',auth(), deleteProduct);
+router.delete('/removeProduct',auth([Roles.Admin]), deleteProduct);
 //router.post('/editProduct', auth(), updateProduct);
 
 
