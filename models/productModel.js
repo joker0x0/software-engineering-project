541e2527bc42798fb40638e6069673bfbdabbde4
joker0x0 <androtechnology733@gmail.com>
2024-05-02 23:20:03 -0700

    added quantity to model

--- models/productModel.js
+++ models/productModel.js
@@ -28,6 +28,10 @@ const productSchema = new mongoose.Schema({
     image:{
         type:String,
     },
+    quantity: {
+        type: Number,
+        requried: true
+    }
 
 },{timestamps:true})
 
