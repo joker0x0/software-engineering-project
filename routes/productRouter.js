8e3dd9cc16e92dfc8e1f9caef3a616470caa74ce
joker0x0 <androtechnology733@gmail.com>
2024-05-02 22:44:25 -0700

    done with update product and show products for ADMIN

--- routes/productRouter.js
+++ routes/productRouter.js
@@ -1,11 +1,11 @@
-const { addProduct, deleteProduct , updateProduct } = require("../controllers/productController")
+const { addProduct, deleteProduct , updateProduct, showProduct } = require("../controllers/productController")
 const router = require("express").Router()
 const { auth ,Roles } = require("../middlewares/authMiddleware");
 
 router.post("/addProduct",auth([Roles.Admin]),addProduct)
 router.post("/deleteProduct",auth([Roles.Admin]),deleteProduct)
-router.post("/editProduct",auth([Roles.Admin]),updateProduct)
-
+router.patch("/editProduct",auth([Roles.Admin]),updateProduct)
+router.get('/showProduct',auth([Roles.Admin]), showProduct)
 
 
 module.exports=router
